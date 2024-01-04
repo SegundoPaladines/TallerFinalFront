b544716b5d930f043d215d353934c2f3b18f2009
SegundoPaladines <segundopaladines@gmail.com>
2024-01-03 22:43:11 -0500

    Buscador

--- src/pages/mascotas/index.js
+++ src/pages/mascotas/index.js
@@ -2,6 +2,7 @@ import { useAuth, useMascotas } from "../../providers";
 import { DefaultLayout } from "../../common";
 import { useEffect, useState } from "react";
 import { MascotaComponent, MascotasFormComponent } from "../../components";
+import "./index.css";
 
 const Mascotas = () => {
     //providers
@@ -10,15 +11,30 @@ const Mascotas = () => {
 
     //listas
     const [mascotas, setMascotas] = useState([]);
+    const [busqueda, setBusqueda] = useState([]);
 
     useEffect(()=>{
        getMascotas();
     },[]);
 
+    const buscar = async (busqueda) => {
+        if(busqueda && mascotas){
+            setBusqueda(mascotas.filter(mascota => (
+                    (mascota.nombre.toLowerCase().indexOf(busqueda.toLowerCase())) !== -1) ||
+                    (mascota.rasa.toLowerCase().indexOf(busqueda.toLowerCase()) !== -1) ||
+                    (mascota.edad.toString().indexOf(busqueda) !== -1)
+                )
+            );
+        }else{
+            getMascotas();
+        }
+    }
+
     const getMascotas = async() => {
         const res = await mascotasProvider.getMascotas();
         if(res){
             setMascotas(res);
+            setBusqueda(res);
         }
     }
 
@@ -26,13 +42,31 @@ const Mascotas = () => {
         <DefaultLayout>
             <div className="mt-5 pt-4">
                 <div className="container">
+                    <div className="row container-buscador">
+                        <div className="input-group">
+                            <span className="icono-buscador" id="buscador-span">
+                                <i className="fa-solid fa-search"></i>
+                            </span>
+                            <input
+                                className="form-control"
+                                type="text"  
+                                aria-label="buscador" 
+                                aria-describedby="buscador-span"
+                                id="buscador"
+                                placeholder="Nombre, Edad o Raza ..."
+                                onChange={(e) => {
+                                    buscar(e.target.value);
+                                }}
+                            />
+                        </div>
+                    </div>
                     {   
                         auth.user.rol==="administrador"?(
-                            <div className="row">
+                            <div className="row mb-3">
                                 <div className="col">
                                     <button 
                                         type="button"
-                                        className="btn btn-outline-success w-100 mb-3"
+                                        className="btn btn-outline-success w-100"
                                         data-bs-toggle="modal"
                                         data-bs-target={`#mascota-formulario-Agregar-`}
                                     >
@@ -44,8 +78,8 @@ const Mascotas = () => {
                     }
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
                         {
-                            mascotas?(
-                                mascotas.map((mascota) => (
+                            busqueda?(
+                                busqueda.map((mascota) => (
                                     <div className="col mb-5"  key={`maascota${mascota.pk}`}>
                                             <MascotaComponent
                                                 {...mascota}
